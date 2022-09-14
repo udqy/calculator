56e6a805a54203481a95a9fe23cdd72cebee2b10
uday <udaygitlog@gmail.com>
2022-09-14 11:24:32 +0530

    Create basic functions

---
+++ script.js
@@ -0,0 +1,21 @@
+//create operators
+function add(a,b) { return Math.round((a+b)*100)/100; };
+
+function subtract(a,b) { return Math.round((a-b)*100)/100; };
+
+function multiply(a,b) { return Math.round((a*b)*100)/100; }; 
+
+function divide(a,b) {
+    if (a===0) return 'undefined';
+    if (b===0) return 'undefined';
+    return Math.round((a/b)*100)/100;
+};
+
+//the main function
+function operator(operator,a,b) {
+    if (operator == '+') return add(a,b);
+    if (operator == '-') return subtract(a,b);
+    if (operator == '*') return multiply(a,b);
+    if (operator == '/') return divide(a,b);
+    else return "Incorrect operator, try again";
+}
